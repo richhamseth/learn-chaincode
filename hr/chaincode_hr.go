05cbbd2eb6ebb1a002021bf67cd3b18052bb6081
Ubuntu <ubuntu@ip-172-31-93-86.ec2.internal>
2019-05-30 12:14:25 +0000

    updated

--- hr/chaincode_hr.go
+++ hr/chaincode_hr.go
@@ -84,27 +84,43 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 
 // Query is our entry point for queries
 func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, id []string) ([]byte, error) {
-	var jsonResp string
-	var key string
-	var err error
+	// var jsonResp string
+	// var key string
+	// var err error
 
 	if len(id) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 1")
 	}
 
-	key = id[0]
-
-	valAsbytes, err := stub.GetState(key)
-
-	if err != nil {
-		jsonResp = "{\"Error\":\"Failed to get state for \"}"
-		return nil, errors.New(jsonResp)
+	if function == "read" { //read a variable
+		return t.read(stub, id)
 	}
+	
+	fmt.Println("query did not find func: " + function)
 
-	return valAsbytes, nil					//error
+	return nil, errors.New("Received unknown function query: " + function)					//error
 
 	// return nil, errors.New("Received unknown function query: " + function)
 }
+
+func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var key, jsonResp string
+	var err error
+
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
+	}
+
+	key = args[0]
+	valAsbytes, err := stub.GetState(key)
+	if err != nil {
+		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+
+	return valAsbytes, nil
+}
+
 
 // ============================================================================================================================
 // Main
